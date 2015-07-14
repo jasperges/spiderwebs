248b314abb179cbe2e904652e05f362a21e61f5b
jasperge <jaspervannieuwenhuizen@gmail.com>
2015-07-14 15:23:28 +0200

    Added drape parameter and seed for generation

--- add_curve_spiderwebs.py
+++ add_curve_spiderwebs.py
@@ -24,90 +24,6 @@ from bpy.props import (IntProperty,
                        BoolProperty)
 
 
-def create_psystem(obj, count):
-    name = "cobwebs"
-    p_modifier = obj.modifiers.new(name, 'PARTICLE_SYSTEM')
-    p_system = p_modifier.particle_system
-    p_settings = p_system.settings
-    p_system.name = p_settings.name = name
-    # p_system.seed = seed
-    p_settings.count = count
-    p_settings.frame_start = p_settings.frame_end = 0
-    p_settings.physics_type = 'NO'
-    # p_locations = [p.location for p in p_system.particles]
-    # obj.modifiers.remove(p_modifier)
-    # return p_locations
-    return p_system
-
-
-def join_objects(objects):
-    scene = bpy.context.scene
-    ctx = bpy.context.copy()
-    # one of the objects to join
-    ctx['active_object'] = objects[0]
-    ctx['selected_objects'] = objects
-    # we need the scene bases as well for joining
-    ctx['selected_editable_bases'] = [scene.object_bases[obj.name] for obj in objects]
-    bpy.ops.object.join(ctx)
-    return objects[0]
-
-
-def main(context, self):
-    selected_objects = bpy.context.selected_objects
-    old_objects = [obj for obj in selected_objects if obj.type == 'MESH']
-    end_positions = []
-    num_main_strands = self.amount
-    main_iter = self.main_iterations
-    # sub_iter = self.sub_iterations
-
-    scene = context.scene
-
-    # Step 1 + 2: Create particles to use for 'main strand' end positions
-    #             Collect the locations of all particles
-    if not old_objects:
-        return
-    # Make a copy of the selected objects and join them
-    copy_objects = [obj.copy() for obj in old_objects]
-    for obj in copy_objects:
-        obj.data = obj.data.copy()
-        scene.objects.link(obj)
-    particle_object = join_objects(copy_objects)
-    p_system = create_psystem(particle_object, num_main_strands)
-    # Update scene for particles to be there during script run
-    scene.update()
-    end_positions = [p.location for p in p_system.particles]
-
-    # Step 3: Create "Main strands" using particles for end positions
-    if not len(end_positions) > 1:
-        return
-    curve = bpy.data.curves.new("cobweb", 'CURVE')
-    curve.dimensions = '3D'
-    curve.fill_mode = 'FULL'
-    for _ in range(main_iter):
-        for i, loc in enumerate(end_positions):
-            points = end_positions[:]
-            points.pop(i)
-            spline = curve.splines.new('NURBS')
-            spline.points.add(count=3)
-            loc1 = loc
-            loc4 = random.choice(points)
-            loc2 = loc1 + (loc4 - loc1) * .3
-            loc3 = loc1 + (loc4 - loc1) * .6
-            # loc2.z -= random.random() * 2 + .2
-            # loc3.z -= random.random() * 2 + .2
-            spline.points[0].co = loc1.to_4d()
-            spline.points[1].co = loc2.to_4d()
-            spline.points[2].co = loc3.to_4d()
-            spline.points[3].co = loc4.to_4d()
-            spline.use_endpoint_u = True
-            spline.order_u = 4
-    cobweb = bpy.data.objects.new("cobweb", curve)
-    bpy.context.scene.objects.link(cobweb)
-
-    scene.objects.unlink(particle_object)
-    bpy.data.objects.remove(particle_object)
-
-
 class Spiderweb(bpy.types.Operator):
     """Add a spiderweb (or wires) between the selected objects."""
     bl_idname = "curve.spiderweb"
@@ -140,17 +56,22 @@ class Spiderweb(bpy.types.Operator):
                                  min=0,
                                  max=999999,
                                  soft_max=100)
+    seed = IntProperty(name="Seed",
+                       description="The seed to use for the generation "
+                                   "(change it to get a different variant "
+                                   "of the web)",
+                       default=0)
     drape_min = FloatProperty(name="Drape min",
                               description="The minimum drape of the strands",
-                              default=-1.0,
-                              soft_min=-10.0,
-                              soft_max=10.0,
+                              default=-5.0,
+                              soft_min=-50.0,
+                              soft_max=50.0,
                               step=10)
     drape_max = FloatProperty(name="Drape max",
                               description="The maximum drape of the strands",
                               default=0,
-                              soft_min=-10.0,
-                              soft_max=10.0,
+                              soft_min=-50.0,
+                              soft_max=50.0,
                               step=10)
     length_solver = BoolProperty(name="Length solver",
                                  description="Length solver",
@@ -170,6 +91,7 @@ class Spiderweb(bpy.types.Operator):
         box.prop(self, 'include_ends')
         box.prop(self, 'sub_iterations')
         box = layout.box()
+        box.prop(self, 'seed')
         box.prop(self, 'drape_min')
         box.prop(self, 'drape_max')
         box.prop(self, 'length_solver')
@@ -177,13 +99,109 @@ class Spiderweb(bpy.types.Operator):
     # Poll
     @classmethod
     def poll(cls, context):
-        return (context.selected_objects and
-                context.scene.active_object.type == 'MESH')
+        if context.selected_objects:
+            for obj in context.selected_objects:
+                if obj.type == 'MESH':
+                    return True
+
+    # Helper functions
+    def create_psystem(self, obj):
+        name = "cobwebs"
+        p_modifier = obj.modifiers.new(name, 'PARTICLE_SYSTEM')
+        p_system = p_modifier.particle_system
+        p_settings = p_system.settings
+        p_system.name = p_settings.name = name
+        p_system.seed = self.seed * 1979
+        p_settings.count = self.amount
+        p_settings.frame_start = p_settings.frame_end = 0
+        p_settings.physics_type = 'NO'
+        return p_system
+
+    def join_objects(self, objects):
+        scene = bpy.context.scene
+        ctx = bpy.context.copy()
+        # One of the objects to join.
+        ctx['active_object'] = objects[0]
+        ctx['selected_objects'] = objects
+        # We need the scene bases as well for joining.
+        ctx['selected_editable_bases'] = [scene.object_bases[obj.name] for obj in objects]
+        bpy.ops.object.join(ctx)
+        return objects[0]
+
+    def spline_length(self, spline):
+        # Only works for straight (sp)lines.
+        return (spline.points[-1].co - spline.points[0].co).length
+
+    def average_spline_length(self, curve):
+        # Only works for straight (sp)lines.
+        total_length = 0
+        for spline in curve.splines:
+            total_length += self.spline_length(spline)
+        return total_length / len(curve.splines)
+
+    def drape_web(self, object):
+        for i, spline in enumerate(object.data.splines):
+            random.seed(self.seed + i * 100)
+            drape = random.uniform(self.drape_min, self.drape_max)
+            length = self.spline_length(spline)
+            average_length = self.average_spline_length(object.data)
+            if self.length_solver:
+                drape = length * drape / average_length
+            spline.points[1].co.z += drape
 
     # Execute
     def execute(self, context):
-        # Run main function.
-        main(context, self)
+        selected_objects = bpy.context.selected_objects
+        old_objects = [obj for obj in selected_objects if obj.type == 'MESH']
+        end_positions = []
+        scene = context.scene
+
+        # Step 1 + 2: Create particles to use for 'main strand' end positions
+        #             Collect the locations of all particles
+        if not old_objects:
+            return
+        # Make a copy of the selected objects and join them
+        copy_objects = [obj.copy() for obj in old_objects]
+        for obj in copy_objects:
+            obj.data = obj.data.copy()
+            scene.objects.link(obj)
+        particle_object = self.join_objects(copy_objects)
+        p_system = self.create_psystem(particle_object)
+        # Update scene for particles to be there during script run
+        scene.update()
+        end_positions = [p.location for p in p_system.particles]
+
+        # Step 3: Create "Main strands" using particles for end positions
+        if not len(end_positions) > 1:
+            return
+        curve = bpy.data.curves.new("cobweb", 'CURVE')
+        curve.dimensions = '3D'
+        curve.fill_mode = 'FULL'
+        for i in range(self.main_iterations):
+            for ip, loc in enumerate(end_positions):
+                points = end_positions[:]
+                points.pop(ip)
+                spline = curve.splines.new('NURBS')
+                spline.points.add(count=2)
+                loc1 = loc
+                random.seed(self.seed + i * ip * 100)
+                loc3 = random.choice(points)
+                loc2 = loc + ((loc3 - loc) * .5)
+                spline.points[0].co = loc1.to_4d()
+                spline.points[1].co = loc2.to_4d()
+                spline.points[2].co = loc3.to_4d()
+                spline.use_endpoint_u = True
+                spline.order_u = 3
+        cobweb = bpy.data.objects.new("cobweb", curve)
+        bpy.context.scene.objects.link(cobweb)
+        # Delete the object with the particle system, we don't need it anymore.
+        scene.objects.unlink(particle_object)
+        bpy.data.objects.remove(particle_object)
+        # Add some drape to the splines.
+        self.drape_web(cobweb)
+        # Select the cobweb and make it the active object.
+        cobweb.select = True
+        bpy.context.scene.objects.active = cobweb
 
         return {'FINISHED'}
 
